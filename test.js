a1677301ed9d6952c255723467dcf4f9659b07a5
FactorSef <aaykovenko@yandex.ru>
2022-03-25 20:47:10 +0400

    jsdf

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log('Hello, world!!!!');
-console.log('Hello, world!!!!');
+console.log('He123124124llo, world!!!!');
 console.log('Hello, world!!!!');
 console.log('Hello, world!!!!');
 console.log('Hello, world!!!!');
