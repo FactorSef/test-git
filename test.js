fef6add4380cc71fc075bf47a52f96270fe7ae89
FactorSef <aaykovenko@yandex.ru>
2022-03-25 20:55:57 +0400

    hot fix

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 console.log('Hello, world!!!!');
-console.log('He123124124llo, world!!!!');
 console.log('Hello, world!!!!');
 console.log('Hello, world!!!!');
-console.log('Helsdkfksldfklsdfllksfdlksdflo, world!!!!');
 console.log('Hello, world!!!!');
 console.log('Hello, world!!!!');
